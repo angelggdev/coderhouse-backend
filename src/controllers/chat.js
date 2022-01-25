427ef77759c72dea2053ea9b6e4a6fbe8e493c71
Angel <angelggdev@gmail.com>
2022-01-25 18:17:45 -0300

    se reformula formato de mensajes

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -8,10 +8,11 @@ function render(data) {
     if (data.length > 0) {
         data.forEach((info) => {
             $('#messages').append(`
-                <div>
-                    <strong style="color:blue">${info.author}</strong>
+                <div style="display:flex; align-items:center; gap: 5px;">
+                    <strong style="color:blue">${info.author.id}</strong>
                     <span style="color:brown">[${info.time}]</span> 
                     <i style="color:green">${info.text}</i>
+                    <img src=${info.author.avatar} alt="foto de perfil" style="width: 50px; height: 50px">
                 </div>
             `);
         });
@@ -21,12 +22,25 @@ function render(data) {
 $('#myForm').submit((e) => {
     e.preventDefault();
 
-    let username = $('#username').val();
+    const username = $('#username').val();
+    const name = $('#name').val();
+    const lastName = $('#lastName').val();
+    const age = $('#age').val();
+    const alias = $('#alias').val();
+    const avatar = $('#avatar').val();
+    const text = $('#texto').val();
 
     if (username) {
         const message = {
-            author: username,
-            text: $('#texto').val(),
+            author: {
+                id: username,
+                name: name, 
+                lastName: lastName,
+                age: age,
+                alias: alias,
+                avatar: avatar
+            },
+            text: text,
         };
         chatSocket.emit('new-message', message);
 
