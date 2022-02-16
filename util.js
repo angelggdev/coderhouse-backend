b14dcafbfecb6f9bce9f42111bbfee33e7117491
Angel <angelggdev@gmail.com>
2022-02-16 13:00:21 -0300

    configuracion de util.js

--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 const env = require('dotenv').config();
 
 module.exports = {
-    DB_URL: process.env.parsed.DB_URL,
-    SESSION_URL: process.env.parsed.SESSION_URL
+    DB_URL: process.env.DB_URL,
+    SESSION_URL: process.env.SESSION_URL
 }
